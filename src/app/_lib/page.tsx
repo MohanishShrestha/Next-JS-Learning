fd42c5c8ccb4df21ba9bf35e48fa6619f17e49af
Mohanish <shresthamohanish123@gmail.com>
2025-12-12 10:27:37 +0545

    second commit

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRoute() {
+  return <h1>This is a private route. You must be logged in to view this page.</h1>;
+}
--- src/app/products/[productid]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,13 @@
+import { notFound } from "next/navigation";
+import { parse } from "path";
+
 export default async function ProductReviews({params,}: {
     params: Promise<{ productid: string; reviewId: string}>
 }){
-    const {productid, reviewId} = await params
+    const {productid, reviewId} = await params;
+
+    if(parseInt(reviewId) > 1000){
+        notFound();
+    }
 return <h1>ReviewId {reviewId} for product {productid}</h1>
 }
